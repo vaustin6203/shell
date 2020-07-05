9a7d5c8b24d352fe0ae9561604c7cad90ba7497e
victoria austin <vaustin@berkeley.edu>
2020-07-05 20:25:42 +0000

    fixed output?

--- shell.c
+++ shell.c
@@ -205,13 +205,13 @@ int main(unused int argc, unused char *argv[]) {
 	      	int index = redirect_output(tokens);
 		if (index > 0) {
 		    char* file = tokens_get_token(tokens, index);
-		    int out = open(file, O_WRONLY | O_CREAT | O_TRUNC);
+		    int out = open(file, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 		    if (out == -1) {
 			perror("Cannot open output file\n");
                         return 0;
 		    } else {
+			fflush(stdout);
 			dup2(out, 1);
-			close(out);
 			execv(args[0], args);
 		    }
 		} else {
